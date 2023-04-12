25509d0f08574fc4ce4a2c551f84260b7775eb97
Vyacheslav Kart <KartashovVD@yandex.ru>
2023-04-13 00:04:05 +0700

    last task done

---
+++ HW1/task008.py
@@ -0,0 +1,26 @@
+# Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника). 
+
+
+def check_break_off_possibility(rows, columns, desired_size):
+    if desired_size < rows and desired_size < columns:
+        return False
+    horizontal = check_horizontal_break_off_possibility(rows, columns, desired_size)
+    print(horizontal)
+    vertical = check_horizontal_break_off_possibility(columns, rows, desired_size)
+    print(vertical)
+    return horizontal or vertical
+
+
+def check_horizontal_break_off_possibility(rows, columns, desired_size):
+    current_possible_range = 0
+    for i in range(rows):
+        current_possible_range += columns
+        if (current_possible_range == desired_size): 
+            return True
+        elif (current_possible_range > desired_size):
+            return False
+    return False
+
+
+if __name__ == '__main__':
+    print(check_break_off_possibility(3, 2, 4))
